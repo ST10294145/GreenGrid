192f0bdda79647f61a19fdd24f490ff832f0fbca
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-05-13 09:56:38 +0200

    Updated model

--- Models/User.cs
+++ Models/User.cs
@@ -5,18 +5,22 @@ namespace GreenGrid.Models
     public class User
     {
         public int Id { get; set; }
+
         [Required]
         public string Name { get; set; }
 
-        [Required]
-        [EmailAddress]
+        [Required, EmailAddress]
         public string Email { get; set; }
-        [Required]
-        public string PasswordHash { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }  
+
         [Required]
         public string Province { get; set; }
+
         [Required]
         public string Department { get; set; }
+
         [Required]
         public string Role { get; set; } // "Farmer" or "Employee"
     }
