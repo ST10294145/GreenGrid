73249ce9635e571c6f7b414450ee7d983083f6a7
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-05-13 09:56:23 +0200

    Added controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,60 @@
+﻿using GreenGrid.Data;
+using GreenGrid.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace GreenGrid.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction("Login");
+            }
+
+            return View(user);
+        }
+
+        // GET: /Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Email == email && u.Password == password);
+
+            if (user != null)
+            {
+                // Store role in session (optional for now)
+                HttpContext.Session.SetString("UserRole", user.Role);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Invalid login credentials.";
+            return View();
+        }
+    }
+}
