e01e41cb75004265b157c7dc8b92a1ee3ec0ee4a
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-05-13 15:04:31 +0200

    Updated User model.

--- Models/User.cs
+++ Models/User.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using GreenGrid.Models;
 
 namespace GreenGrid.Models
 {
-    public class User
+    public class User : IdentityUser
     {
         public int Id { get; set; }
 
