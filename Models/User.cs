e19aedfa1e1b452393bb2f5e221e114d61ed752a
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-05-13 09:41:21 +0200

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GreenGrid.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+        [Required]
+        public string Province { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
+        public string Role { get; set; } // "Farmer" or "Employee"
+    }
+}
