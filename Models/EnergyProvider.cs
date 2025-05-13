aa6ae1222e8d7bc6ae2564245f2a5f418c0d39f7
Saihil Gurupersad <ST10294145@vcconnect.edu.za>
2025-05-13 21:33:55 +0200

    New model energy provider

---
+++ Models/EnergyProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GreenGrid.Models
+{
+    public class EnergyProvider
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+
+        [Required]
+        public string ServiceType { get; set; } // e.g., Solar, Wind, Biogas
+
+        [Required]
+        public string Province { get; set; }
+
+        [Required]
+        public string ContactEmail { get; set; }
+
+        public string Description { get; set; }
+
+        public string Website { get; set; }
+    }
+
+}
